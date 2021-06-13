81fdd5307efb0d34cf22e20d0696fa337befb010
Tim Randall <trtimrandall@gmail.com>
2021-06-13 16:42:43 +1000

    Initial Commit

---
+++ StringEditor/StringEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringEditorApp.swift
+//  StringEditor
+//
+//  Created by Tim Randall on 13/6/21.
+//
+
+import SwiftUI
+
+@main
+struct StringEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
